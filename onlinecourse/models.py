597d90046f0a0e2476a9a46dec7fab2b784d29e0
scd346 <StephenCDoherty@gmail.com>
2021-11-11 16:53:12 +0000

    submission uncommented

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -136,10 +136,7 @@ class Choice(models.Model):
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    chocies = models.ManyToManyField(Choice)
-#    Other fields and methods you would like to design
 class Submission(models.Model):
-    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-    choices = models.ManyToManyField(Choice)
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   chocies = models.ManyToManyField(Choice)
+#    Other fields and methods you would like to design
