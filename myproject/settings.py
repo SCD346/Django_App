da83ba597403a292f8e91ee16fa5ee3c77852c60
steve_D <scd346@nyu.edu>
2021-11-16 19:19:29 -0600

    routes and urls updates

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'aay0j_9b&ky3a7(8m8il+-1ud(scw12@w5!+5-=gsk6ynzi0ls'
 DEBUG = True
 
 # <HINT> add your cloud host here
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'onlinecourse321.us-south.cf.appdomain.cloud'
+]
 
 
 # Application definition
@@ -124,4 +126,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
-
